5104093123a1a1cf6b9e08d9ee90c7e28b4f3c39
jaynedoe <jaedoyne@hotmail.com>
2020-12-05 07:09:45 +1100

    completed easy1

--- easy1.js
+++ easy1.js
@@ -1,32 +1,56 @@
-
 const numberArray = [3, 5, -4, 8, 11, 1, -1, 6];
 const target = 10;
 
+let randomArray = [];
+
+for (i = 0; i < 10; i++) {
+  let randomNumber = Math.floor(Math.random() * 10);
+  randomArray.push(randomNumber);
+}
+
+let newTarget = Math.floor(Math.random() * 10);
+
+//FIRST SOLUTION
+
 function twoNumberSum(array, targetSum) {
-  
   //declare an empty array variable to store the two array indexes
   let result = [];
 
   //for each item in the number array, find out if the difference between the number and the target is also in the array
   //if a number representing the difference is in the array, then the numbered index of that number will be somewhere between 0 - array.length
-  
-  array.forEach(function(item, index){
-    let difference = targetSum - item;
+
+  for (let i = 0; i < array.length; i++) {
+    let difference = targetSum - array[i];
     let differenceIndex = array.indexOf(difference);
 
-  //if the number representing the difference is not in the array, the indexOf will be -1
-  //to prevent using the same number twice, include an additional AND condition that checks that the differenceIndex does not equal the current index iteration  
-  //if the conditions are met and a 'difference' is found that isn't also the current index, push that into the results array.
-  //I'm not sure why this is returning two indexes instead of one?
-
-    if(differenceIndex > -1 && differenceIndex !== index){
-      result.push(index);
-     // result.push(differenceIndex);
+    if (differenceIndex > -1 && differenceIndex !== i) {
+      result.push(array[i]);
+      result.push(array[differenceIndex]);
+      break;
     }
-  });
+  }
 
   return result;
 }
 
-console.log(twoNumberSum(numberArray, target));
+// console.log(twoNumberSum(numberArray, target));
+// console.log(`Random array was: ${numberArray} and the target was ${target}`);
 
+
+// SECOND SOLUTION
+
+function twoNumberSumming(array, targetSum) {
+  for (let i = 0; i < array.length - 1; i++) {
+		const firstNum = array[i];
+		for (let j = i + 1; j < array.length; j++){
+			const secondNum = array[j];
+			if(firstNum+secondNum === targetSum){
+				return [firstNum, secondNum];
+			}
+		}
+	}
+	return [];
+}
+
+console.log(twoNumberSumming(randomArray, newTarget));
+console.log(`Random array was: ${randomArray} and the target was ${newTarget}`);
